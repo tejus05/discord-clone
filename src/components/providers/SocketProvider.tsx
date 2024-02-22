1c5b6900c35607ac4777178b79c0d93e677389fe
tejus <thequantumcoder@gmail.com>
2024-02-22 12:48:47 +0530

    add a seperate stateful express backend

--- src/components/providers/SocketProvider.tsx
+++ src/components/providers/SocketProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
-import { io as ClientIO } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 
 type SocketContextType = {
-  socket: any | null;
+  socket: Socket | null;
   isConnected: boolean;
 };
 
@@ -18,30 +18,31 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const [socket, setSocket] = useState(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
-    const socketInstance = new (ClientIO as any)(
-      process.env.NEXT_PUBLIC_SITE_URL!,
-      {
-        path: "/api/socket/io",
-        addTrailingSlash: false,
-      }
-    );
+    let newSocket: Socket;
 
-    socketInstance.on("connect", () => {
+    const URL =
+      process.env.NODE_ENV === "production"
+        ? undefined
+        : "http://localhost:4000";
+    
+    newSocket = io(URL as string);
+
+    newSocket.on("connect", () => {
       setIsConnected(true);
     });
 
-    socketInstance.on("disconnect", () => {
+    newSocket.on("disconnect", () => {
       setIsConnected(false);
     });
 
-    setSocket(socketInstance);
+    setSocket(newSocket);
 
     return () => {
-      socketInstance.disconnect();
+      newSocket.disconnect();
     };
   }, []);
 
